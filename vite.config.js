4e38489e8a01f3e0ad4a0248ab01cc99ad60858f
sreeshanth09-prog <sreeshanth099@gmail.com>
2025-11-10 15:35:04 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,21 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+
+  // GitHub Pages base URL
   base: "/ecobin/",
 
+  server: {
+    host: true,       // allow external access (required for Render)
+    port: 5173        // Vite default port
+  },
+
+  preview: {
+    host: "0.0.0.0",  // Render requires public host
+    port: 10000        // Must match render start command
+  },
+
   build: {
-    chunkSizeWarningLimit: 2000, // increase warning limit (in KB)
+    chunkSizeWarningLimit: 2000,
   }
 });
